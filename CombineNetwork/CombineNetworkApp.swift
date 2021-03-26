af583ab30a953331fb9440969a56526e5e965fc9
phdafoe <phdafoe@gmail.com>
2021-03-26 11:54:36 +0100

    Initial Commit

---
+++ CombineNetwork/CombineNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineNetworkApp.swift
+//  CombineNetwork
+//
+//  Created by Andres Felipe Ocampo Eljaiek on 26/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct CombineNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
